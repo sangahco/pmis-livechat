e57b19a8cb29a946166f030cae39e8cdf1008a3c
Emanuele Disco <emawind84@gmail.com>
2018-08-02 14:53:03 +0900

    material-design ui upgrade

--- client/app.module.js
+++ client/app.module.js
@@ -46,4 +46,36 @@
 
     }]);
 
+    app.run(function ($rootScope,$timeout) {
+        $rootScope.$on('$viewContentLoaded', ()=> {
+            $timeout(() => {
+                componentHandler.upgradeAllRegistered();
+            })
+        })
+    });
+
+    app.run(function () {
+        var mdlUpgradeDom = false;
+        setInterval(function() {
+            if (mdlUpgradeDom) {
+                //console.log('upgrading dom...');
+                componentHandler.upgradeDom();
+                mdlUpgradeDom = false;
+            }
+        }, 200);
+    
+        var observer = new MutationObserver(function () {
+            mdlUpgradeDom = true;
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+        /* support <= IE 10
+        angular.element(document).bind('DOMNodeInserted', function(e) {
+            mdlUpgradeDom = true;
+        });
+        */
+    });
+
 })();
