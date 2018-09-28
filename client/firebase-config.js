b8ca484b80a78bf17902745cad53a6097373390d
Emanuele Disco <emawind84@gmail.com>
2018-09-28 14:27:28 +0900

    update

---
+++ client/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "",
+    authDomain: "friendlychat-41e71.firebaseapp.com",
+    databaseURL: "https://friendlychat-41e71.firebaseio.com",
+    projectId: "friendlychat-41e71",
+    storageBucket: "friendlychat-41e71.appspot.com",
+    messagingSenderId: "252271530907"
+};
+firebase.initializeApp(config);
