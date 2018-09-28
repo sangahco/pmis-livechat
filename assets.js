a6f63c270ffd42b131e14fe1bcb825a697533695
Emanuele Disco <emawind84@gmail.com>
2018-09-28 18:18:24 +0900

    migrated bower to webpack

---
+++ assets.js
@@ -0,0 +1,16 @@
+const CSS = [
+    
+];
+const JS = [
+    'angular/angular.min.js',
+    'angular/angular.min.js.map',
+    'angular-route/angular-route.min.js',
+    'angular-route/angular-route.min.js.map',
+    'jquery/dist/jquery.min.js',
+    'jquery/dist/jquery.min.map',
+    'socket.io-client/dist/socket.io.slim.js',
+    'socket.io-client/dist/socket.io.slim.js.map',
+    'datalist-polyfill/datalist-polyfill.min.js'
+];
+
+module.exports = [...JS, ...CSS];
